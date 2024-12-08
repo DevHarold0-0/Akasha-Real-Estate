96e0326a2224ac3b663014685342a69009070935
Harold <155407111+DevHarold0-0@users.noreply.github.com>
2024-12-07 21:28:11 -0500

    Fix syntax bug

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -152,7 +152,7 @@ const Task = ({ task }: TaskProps) => {
           ? "bg-red-200 text-red-700"
           : priority === "High"
             ? "bg-yellow-200 text-yellow-700"
-            : priority === "Medium"
+            : priority === "Meduim"
               ? "bg-green-200 text-green-700"
               : priority === "Low"
                 ? "bg-blue-200 text-blue-700"
